afcd6b7063ac4cb491d94b5a4a635782c7772be1
ziv <ziv@root-nt.com>
2020-12-25 12:46:30 +0200

    added funcs/class docs

--- LAB5/lab5.py
+++ LAB5/lab5.py
@@ -17,39 +17,80 @@ from LAB5.neural_network import NeuralNetwork as NN
 MAX_INPUT = 8
 
 
-def get_system_input(number_of_bits):
-    system_input = numpy.reshape(numpy.arange(0, 2**number_of_bits, dtype=numpy.uint8), (-1, 1))
-    return numpy.unpackbits(system_input, axis=1, bitorder='little', count=number_of_bits)
+def get_bits_vectors(number_of_bits):
+    """
+    Generate array-like of arrays with the shape(index, bit_vector)
+    with all the bit combination with the length of number_of_bits
+
+    Parameters
+    ----------
+    number_of_bits : int
+        number of bits in the vector
+
+    Returns
+    -------
+    : array-like of array
+        array with all bit combination vectors
+        with the shape (2**number_of_bits, number_of_bits)
+    """
+    bits_vectors = numpy.reshape(numpy.arange(0, 2**number_of_bits, dtype=numpy.uint8), (-1, 1))
+    return numpy.unpackbits(bits_vectors, axis=1, bitorder='little', count=number_of_bits)
 
 
-def get_system_output(x, operation=operator.xor):
-    return numpy.reshape(numpy.asarray([reduce(operation, x[i]) for i in range(MAX_INPUT)]), (-1, 1))
+def get_operation_bit_vector_output(x, operation=operator.xor):
+    """
+    Generate array-like of arrays with the shape(index, bit_vector)
+    with all the bit combination with the length of number_of_bits
+
+    Parameters
+    ----------
+    x : array-like of array
+        array of bit combination vectors
+
+    Optional Parameters
+    -------------------
+    operation : operator-type
+        operator between the bits, default xor.
+        for more information
+        please read operator.py (python built in)
+
+    Returns
+    -------
+    : array-like of array
+        array with output of the operator for all bits
+        with the shape (2**number_of_bits, output)
+    """
+    return numpy.reshape(numpy.asarray([reduce(operation, x[index]) for index in range(MAX_INPUT)]), (-1, 1))
 
 
 if __name__ == "__main__":
     plt = lu.get_plt()
+    os.makedirs("results", exist_ok=True)
 
-    x_input = get_system_input(int(numpy.log2(MAX_INPUT)))
-    y_output = get_system_output(x_input)
+    # LAB 5
+    x_input = get_bits_vectors(int(numpy.log2(MAX_INPUT)))
+    y_output = get_operation_bit_vector_output(x_input)
 
     iteration = 100
     hidden_layer_nodes = 3
 
     for q in range(1, 3):
         hidden_layer_nodes *= q
+        t = numpy.arange(1, 101)
         error = []
-        t = []
-        for i in range(iteration):
+        for i in range(1, iteration + 1):
             n_net = NN(x_input, y_output, hidden_layer_nodes)
-            n_net.train(epochs=2000)
+            n_net.train(epochs=2000, learning_rate=2)
             error.append(numpy.square(n_net.error).mean())
-            t.append(i)
         plt.figure(q)
-        plt.title(f'MSE over {iteration} iterations\nwith {hidden_layer_nodes} nodes (Hidden layer)')
+        plt.title(f'MSE over {iteration} iterations\nHidden layer with {hidden_layer_nodes} nodes')
         plt.xlabel('iteration')
         plt.ylabel('MSE')
+        plt.xlim([1, iteration])
         plt.ylim([0, 0.5])
         plt.plot(t, error)
         plt.grid(True)
-    plt.show()
+        plt.savefig(f"results/MSE_for_{hidden_layer_nodes}_nodes")
 
+    # Unmark the command blow to show ALL the figures
+    # plt.show()
--- LAB5/neural_network.py
+++ LAB5/neural_network.py
@@ -9,13 +9,49 @@ import numpy
 
 
 class NeuralNetwork:
+    """
+    A class of Neural Network with 1 hidden layer
 
-    # initialize variables in class
-    def __init__(self, inputs, outputs, layer_nodes):
+    Attributes
+    ----------
+    inputs : array-like of arrays
+        Two-dimensional input (index, input) for training,
+        pairing with output.
+    outputs : array-like of arrays
+        Two-dimensional output (index, output) for training,
+        pairing with input.
+    layer_nodes : int
+        Desired number of neurals in hidden layer, must be natural.
+
+    Raises
+    ------
+    TypeError
+        If number of neurals layer_nodes is not type integer.
+    ValueError
+        If number of of neurals layer_nodes is negative.
+
+    Methods
+    -------
+    _check_input()
+        PRIVATE METHOD: checks the inputs are set correctly
+    sigmoid(x)
+        activation function
+    feed_forward()
+        feeding the Neural Network
+    back_propagation(learning_rate)
+        update weights of the Neural Network
+    train(epochs=10000, learning_rate=1)
+        train the Neural Network epochs times
+    predict(new_input)
+        predict the output for new_input
+    """
+
+    def __init__(self, inputs, outputs=None, layer_nodes=1):
         # initialize inputs, outputs and error for training
         self.inputs = inputs
         self.outputs = outputs
         self.error = None
+        self._check_input(layer_nodes)
         # initialize layers
         self.h_layer1 = None
         self.o_layer = None
@@ -25,23 +61,63 @@ class NeuralNetwork:
         # initialize biases
         self.bias0 = numpy.random.normal(0, 1, layer_nodes)
         self.bias1 = numpy.random.normal(0, 1)
+        # initialize tracking arrays
         self.error_history = []
         self.epoch_list = []
 
-    # activation function ==> S(x) = 1/1+e^(-x)
+    def _check_input(self, neurals):
+        if self.inputs.shape[0] != self.outputs.shape[0]:
+            raise ValueError('Number of input vectors must be the same as output vectors')
+        if type(neurals) is not int:
+            raise TypeError('Number of neurals must be type integer')
+        elif neurals <= 0:
+            raise ValueError('Number of neurals must be greater than 0')
+
     @staticmethod
     def sigmoid(x, deriv=False):
+        """
+        Perform sigmoid function S(x) = 1/1+e^(-x)
+        as activation function of the Neural Network
+
+        Parameters
+        ----------
+        x : int, array-like..
+
+        Optional Parameters
+        -------------------
+        deriv : boolean
+            boolean condition for return the derivative,
+            defaults to False.
+
+        Returns
+        -------
+        :float, array-like..
+            Calculation of sigmoid function,
+            or the derivative of sigmoid function
+        """
         if deriv:
             return x * (1 - x)
         return 1 / (1 + numpy.exp(-x))
 
-    # data will flow through the neural network.
     def feed_forward(self):
+        """
+        Feed forward the input data through the Neural Network,
+        and produce the output.
+        """
         self.h_layer1 = self.sigmoid(numpy.dot(self.inputs, self.weights0) + self.bias0)
         self.o_layer = self.sigmoid(numpy.dot(self.h_layer1, self.weights1) + self.bias1)
 
-    # going backwards through the network to update weights
     def back_propagation(self, learning_rate):
+        """
+        Set the error based the desired output and output layer.
+        Going backwards through the network to make corrections
+        (update weights and bias values), based on the error values.
+
+        Parameters
+        ----------
+        learning_rate : float
+            learning rate of the Neural Network.
+        """
         self.error = self.outputs - self.o_layer
 
         delta1 = self.error * self.sigmoid(self.o_layer, deriv=True)
@@ -50,25 +126,46 @@ class NeuralNetwork:
         delta0 = numpy.dot(self.error, self.weights1.T) * self.sigmoid(self.h_layer1, deriv=True)
         self.weights0 += learning_rate * numpy.dot(self.inputs.T, delta0)
 
-        # Updating the bias weight value
         for b in delta1:
             self.bias1 += learning_rate * b
         for b in delta0:
             self.bias0 += learning_rate * b
 
-    # train the neural net for 2,000 iterations
-    def train(self, epochs=2000, learning_rate=2):
+    def train(self, epochs=10000, learning_rate=1):
+        """
+        Perform training of the Neural Network epochs times
+
+        Optional Parameters
+        -------------------
+        epochs : int
+            number of training iteration, default 10,000.
+        learning_rate: float
+            set learning rate of the Neural Network,
+            default 1.
+        """
         for epoch in range(epochs):
-            # flow forward and produce an output
             self.feed_forward()
-            # go back though the network to make corrections based on the output
             self.back_propagation(learning_rate)
+
             # keep track of the error history over each epoch
             self.error_history.append(numpy.average(numpy.abs(self.error)))
             self.epoch_list.append(epoch)
 
-    # function to predict output on new and unseen input data
     def predict(self, new_input):
+        """
+        Predict output on the new input data via the Neural Network
+
+        Parameters
+        ----------
+        new_input : array-like (N-dimensional)
+            N-dimensional input (index, input) to predict the output.
+
+        Returns
+        -------
+        y : array-like (N-dimensional)
+            N-dimensional output (index, output) of the Neural Network,
+            i.e the prediction.
+        """
         self.h_layer1 = self.sigmoid(numpy.dot(new_input, self.weights0) + self.bias0)
         self.o_layer = self.sigmoid(numpy.dot(self.h_layer1, self.weights1) + self.bias1)
         return self.o_layer
